e7c32df040dc089d7d6f381b466e0ebe73b72fff
Desmo-nd <mwrgdesmond@gmail.com>
2024-07-29 16:24:04 +0300

    set Up

---
+++ src/Screens/Home.js
@@ -0,0 +1,24 @@
+import React from "react";
+import './Home.css';
+
+const Home = () => {
+    return (
+        <div className="Home">
+        <header className="Home-header">
+            <p>
+            Edit <code>src/Screens/Home.js</code> and save to reload.
+            </p>
+            <a
+            className="Home-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+            Learn React
+            </a>
+        </header>
+        </div>
+    );
+};
+
+export default Home;
